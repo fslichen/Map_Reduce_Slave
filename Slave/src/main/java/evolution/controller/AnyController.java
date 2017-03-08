7bc59d52bb3d5df27a46d08c3bce795dff95b58a
fslic <fslic@DESKTOP-MCATCHJ>
2017-03-08 09:45:49 -0800

    new demo

--- Slave/src/main/java/evolution/controller/AnyController.java
+++ Slave/src/main/java/evolution/controller/AnyController.java
@@ -9,14 +9,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import evolution.service.AnyService;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class AnyController {
 	@Autowired
 	private AnyService anyService;
 	
 	@PostMapping("/partialSummarize")
 	public Map<String, Double> partialSummarize(@RequestBody List<String> partialData) {
-		return anyService.partialSummarize(partialData);
+		Map<String, Double> dataCount = anyService.partialSummarize(partialData);
+		log.info("The data count is {}.", dataCount);
+		return dataCount;
 	}
 }
--- Slave/src/test/java/evolution/controller/AnyControllerTest.java
+++ Slave/src/test/java/evolution/controller/AnyControllerTest.java
@@ -12,7 +12,7 @@ import evolution.util.Sender;
 public class AnyControllerTest {
 	@Test
 	public void testPartialSummarize() {
-		Future<HttpResponse> response = Sender.post("http://192.168.0.107:8080/partialSummarize", Arrays.asList("apple", "pear", "apple"));
+		Future<HttpResponse> response = Sender.post("http://localhost:8080/partialSummarize", Arrays.asList("apple", "pear", "apple"));
 		Map<String, Integer> map = Sender.getMap(response, String.class, Integer.class);
 		System.out.println(map);
 	}
